87d865071acb152410d0b32c4c6f366fa831267e
sadelcarpio <sadelcarpioa@gmail.com>
2026-03-08 18:31:41 -0500

    ui hint

---
+++ src/ui.py
@@ -0,0 +1,12 @@
+# Este es el punto de entrada del proyecto. Aqui se define la lógica de cara al usuario
+# Ejemplo: (recomiendo hacer un diagrama de flujo de la experiencia de usuario)
+# Presiona 1 para crear un baúl de contraseñas
+# Presiona 2 para loguearte con una contraseña existente
+# Presiona "a" para agregar un login
+# Presiona "d" para eliminar un login
+# Presiona "e" para editar un login
+# En este modulo se instanciaría el PasswordManager y se llama a todas las funciones o clases necesarias,
+# de acuerdo a los inputs del usuario
+
+if __name__ == "__main__":
+    pass
